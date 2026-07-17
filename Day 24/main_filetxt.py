9ebfb75f614a516687d5e7265a622ef2e7029cf1
Ronaldo <93561911+RonaldoM11@users.noreply.github.com>
2026-07-17 13:26:14 -0700

    Add files via upload

---
+++ Day 24/main_filetxt.py
@@ -0,0 +1,5 @@
+with open("/Users/xXBiSkiTsXx/Desktop/100 Days of Python/Day 24/Outdated/hidden/my_file.txt") as file:
+    Contents = file.read()
+    print(Contents)
+
+#Relative or Absolute file path
