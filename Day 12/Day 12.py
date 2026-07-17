0befbbe76dcf2883bfb8715a8a4009dda3b60a5a
Ronaldo <93561911+RonaldoM11@users.noreply.github.com>
2026-07-17 12:56:37 -0700

    Add files via upload

---
+++ Day 12/Day 12.py
@@ -0,0 +1,91 @@
+#Day 12
+#Namespaces: local vs global scope
+
+enemies = 1
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+#Local Scope
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+
+drink_potion()
+print(potion_strenght)
+
+#Global Scope
+player_health = 10
+
+def game():
+    def drink_potion():
+        potion_strength = 2
+        print(player_health)
+
+    drink_potion()
+print(player_health)
+
+
+
+#There is no block scope in python
+
+
+
+################### Scope ####################
+
+enemies = 1
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+# Local Scope
+
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+
+drink_potion()
+print(potion_strength)
+
+# Global Scope
+player_health = 10
+
+def game():
+    def drink_potion():
+        potion_strength = 2
+        print(player_health)
+
+    drink_potion()
+
+print(player_health)
+
+# There is no Block Scope
+
+game_level = 3
+
+def create_enemy():
+    enemies = ["Skeleton", "Zombie", "Alien"]
+    if game_level < 5:
+        new_enemy = enemies[0]
+
+    print(new_enemy)
+
+
+# Modifying Global Scope
+
+enemies = 1
+
+def increase_enemies():
+    print(f"enemies inside function: {enemies}")
+    return enemies + 1
+
+enemies = increase_enemies()
+print(f"enemies outside function: {enemies}")
