85e525d88fce76ba557eaa88a4b647c32e0c9dba
Ronaldo <93561911+RonaldoM11@users.noreply.github.com>
2026-07-16 18:25:27 -0700

    Add files via upload

---
+++ Day 6/Day-6.py
@@ -0,0 +1,16 @@
+#Day 6
+#Functions
+print("hello")
+num_char = len("hello")
+print(num_char)
+
+def my_function ():
+    print("Hello")
+    print("Bye")
+#this function will not print anything until it is executed.
+my_function() #this closing is what makes the execution occur.
+
+
+#while Loops
+
+
