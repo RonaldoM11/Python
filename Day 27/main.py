099459073d5a643e87f7e70d24faaf5cd33d1484
Ronaldo <93561911+RonaldoM11@users.noreply.github.com>
2026-07-18 19:19:57 -0700

    Add files via upload

---
+++ Day 27/main.py
@@ -0,0 +1,20 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My first GUI Program")
+window.minsize(width=500, height=300)
+
+#Label
+
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack(side="left")
+
+import turtle
+
+tim = turtle.Turtle()
+tim.write("Some text", font=("Times New Roman", 24, "bold"))
+
+
+
+
+window.mainloop()
