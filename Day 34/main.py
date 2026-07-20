0c28b7c09cd0d3178b217e19c122d4f813d612d7
Ronaldo <93561911+RonaldoM11@users.noreply.github.com>
2026-07-20 11:41:03 -0700

    Add files via upload

--- Day 34/main.py
+++ Day 34/main.py
@@ -1,25 +1,15 @@
-age: int
-name: str
-height: float
-is_human: bool
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+from ui import QuizInterface
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
 
-def police_check(age: int) -> bool:
-    if age > 18:
-        can_drive = True
-    else:
-        can_drive = False
-    return can_drive
-
-
-if police_check(12):
-    print("You may pass")
-else:
-    print("Pay a fine.")
-
-
-
-
-
-
-
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
